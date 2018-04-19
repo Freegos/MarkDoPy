13c5d3bc2c97b85e29ae7e1f7363fbb457e9e2f5
Adrien Albaladejo <adrien.albaladejo@gmail.com>
2018-04-19 12:16:31 +0200

    Initial commit

---
+++ markDo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# author: Adrien Albaladejo
+
+# Open source file
+# Open output file
+# Write html header
+# Read each line of source file and parse them
+# When a specific symbol is find (list?), line is send to function who edit it and add html tags
+# The line is added to html file
+# Write html end + close 2 files
+# TADA
